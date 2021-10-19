2fdf10c6a4a19b5c664a9208829f078bd6881c65
Lince Perez <linx002xbox@gmail.com>
2021-10-18 19:05:17 -0500

    Migraciones

---
+++ database/migrations/2021_10_18_221301_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class NewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('News', function (Blueprint $table) {
+            $table->id();
+            $table->string('noticiaTitulo', 100);
+            $table->string('noticiaTexto', 500);
+            $table->string('noticiaTipo', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_10_18_222624_menu_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('Menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('platilloTitulo', 100);
+            $table->string('platilloImagen', 20);
+            $table->string('platilloDescripcion', 250);
+            $table->double('platilloPrecio');
+            $table->double('platilloOferta');
+            $table->boolean('platilloStatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
